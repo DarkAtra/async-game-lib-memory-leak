1b8c323939d3455078bc7f799920d5c71614034b
Tommy Schmidt <tommy.schmidt@idealo.de>
2023-04-22 18:48:36 +0200

    chore: increase the amount of queries to 100000 * 3

--- src/main/kotlin/de/darkatra/agql/Main.kt
+++ src/main/kotlin/de/darkatra/agql/Main.kt
@@ -6,14 +6,14 @@ fun main() {
     val serverHostName = "51.195.60.73"
     val serverQueryPort = 24785
 
-    for (i in 0 until 1000) {
+    for (i in 0 until 100000) {
         runCatching {
 
             val serverInfo = ServerQueryClient.getServerInfo(serverHostName, serverQueryPort)
             val players = ServerQueryClient.getPlayerList(serverHostName, serverQueryPort)
             val rules = ServerQueryClient.getRules(serverHostName, serverQueryPort)
 
-            println("name: ${serverInfo.name}, players: ${players.size}, days-running: ${rules["days-runningv2"]}")
+            println("i: ${i}, name: ${serverInfo.name}, players: ${players.size}, days-running: ${rules["days-runningv2"]}")
         }
     }
 }
